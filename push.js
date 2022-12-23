12b703560a214372ab9571bc1e744ae3f24f374e
alex <alex.lin@itrustwellnessgroup.com>
2022-12-23 23:25:14 +0800

    fix

--- push.js
+++ push.js
@@ -15,6 +15,15 @@ push.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://wns2-ln2p.notify.windows.com/w/?token=BQYAAABapu67JI6qMTcISUYmbqItdyRDFYXyTPO1%2fAYdE32Kz8khSmRYXmK7DgAfgjar5kQKSGLDoRWgUUmf2aPGSt65rtmVbSeR%2bAKYRSaRGdsuo1AWwGg9ZDoN3ww3hfVDD4rLaD%2bzFm07SHANsebbkWeDoD391pIhREmTnpRelYQvzN0DmkMnt7G%2fa86as%2foQb0Bk%2b60kmsVNjNyxGyVD9gCJp%2bT0fGNDAye8irtIYYErj%2bwZTrZWp0qfKCmDxAfDJJwq%2faozdEbNNUFgL8KvUs8vC2vNLA7B9WiNAKytR3t6Frcf7Y%2bzruoH%2bdkbrXbIA3pihMnoZJ7guO6a7enaBGie",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BBFJoqT83s9IEWtoUZu4MCuwUBLbukjfPRR6myIF6VaqI4XbFWK1cSI78uF-CouybI9r8og0vDqZJn8c9usgcl4",
+    auth: "cgI2GF5aVPjL37u_i9WM0A",
+  },
+};
 
 push.sendNotification(sub, "test message");
--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener("push", () => {
-  self.registration.sendNotification();
+self.addEventListener("push", function (e) {
+  var options = {
+    icon: "./assets/icon.png",
+    body: "Welcome to iTrust Wellness Group",
+    image: "./assets/itrust_cover_photo.jpg",
+    requireInteraction: true,
+  };
+  e.waitUntil(
+    self.registration.showNotification("iTrust Wellness Group", options)
+  );
 });
