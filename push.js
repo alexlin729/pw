215f69444ddb58fcd0b1d275b1db201463aac914
alex <alex.lin@itrustwellnessgroup.com>
2022-12-26 20:20:47 +0800

    second test

--- push.js
+++ push.js
@@ -5,8 +5,8 @@ var push = require("web-push");
 
 let vapidKeys = {
   publicKey:
-    "BBEfNgzD34yqRYWjBiZHo7iLIgWxlaoJ9kmXavYhx_2lSuTaXakkYgGoHFPJ8hQzqUKdmCVX_5QU3gOPJkrn2WA",
-  privateKey: "V20jlZ82K4w8drCBvbMqAsqI2RpNaQ2HAvD5j6vvbPE",
+    "BBsRMW9b_dQDD5r0qnldpvlL0bygRSEebg_4TS8_N_0-55GhLAyTEAqSfZSRWXpTeU3DaASTh9V0k9qqUDv_AEw",
+  privateKey: "ikV_DE4apv3zXGxU6sArmLRw5NlBEX5woDuQz1hTfdQ",
 };
 
 push.setVapidDetails(
@@ -14,16 +14,16 @@ push.setVapidDetails(
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
-
-let sub = {
-  endpoint:
-    "https://wns2-ln2p.notify.windows.com/w/?token=BQYAAABapu67JI6qMTcISUYmbqItdyRDFYXyTPO1%2fAYdE32Kz8khSmRYXmK7DgAfgjar5kQKSGLDoRWgUUmf2aPGSt65rtmVbSeR%2bAKYRSaRGdsuo1AWwGg9ZDoN3ww3hfVDD4rLaD%2bzFm07SHANsebbkWeDoD391pIhREmTnpRelYQvzN0DmkMnt7G%2fa86as%2foQb0Bk%2b60kmsVNjNyxGyVD9gCJp%2bT0fGNDAye8irtIYYErj%2bwZTrZWp0qfKCmDxAfDJJwq%2faozdEbNNUFgL8KvUs8vC2vNLA7B9WiNAKytR3t6Frcf7Y%2bzruoH%2bdkbrXbIA3pihMnoZJ7guO6a7enaBGie",
-  expirationTime: null,
-  keys: {
-    p256dh:
-      "BBFJoqT83s9IEWtoUZu4MCuwUBLbukjfPRR6myIF6VaqI4XbFWK1cSI78uF-CouybI9r8og0vDqZJn8c9usgcl4",
-    auth: "cgI2GF5aVPjL37u_i9WM0A",
-  },
-};
+let sub = {};
+// let sub = {
+//   endpoint:
+//     "https://fcm.googleapis.com/fcm/send/dbDvkiZ6q-U:APA91bHcZkv5FuvmgboRCaUcpDgueenj-fM9ATHRx_bfKP4J_KamW319KBgLPNc0zAPQBJdCtR2BfzUTUgGpLKpfZ9o2bN43NDNH_eZbxCiOK9MXXQ0OGGKnw8rICZ_2tfO2NPQR3Nmv",
+//   expirationTime: null,
+//   keys: {
+//     p256dh:
+//       "BMxNCKPux2P5FyzzSUFE_CTYsdJzpFLHjijoKj11HSdVrA_WWK3q2-E-whOfHuD2InTffCI8paaMXmQ9yp5Cx-k",
+//     auth: "7vjvjaxslWoFvV4puFE2WA",
+//   },
+// };
 
 push.sendNotification(sub, "test message");
